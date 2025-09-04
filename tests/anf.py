02b3abf6c5d444279ad97f4ec793602d790584bd
Merlin Fruchon <mfruchon@ptb-02009352.paris.inria.fr>
2025-09-04 10:51:10 +0200

    Ajout du test anf.py

---
+++ tests/anf.py
@@ -0,0 +1,22 @@
+from sage.all import *
+from sboxUv2.core import *
+from sage.crypto.sboxes import sboxes
+
+
+Sb0=Sb(sboxes["Midori_Sb0"])
+anf_Sb0=algebraic_normal_form(Sb0)
+R = PolynomialRing(GF(2), names=('x0', 'x1', 'x2','x3',))
+(x0,x1,x2,x3,) = R._first_ngens(4)
+if anf_Sb0==[x0*x1*x2 + x0*x1*x3 + x0*x2 + x0*x3 + x1*x2*x3 + x1,
+ x0*x2 + x0*x3 + x0 + x2*x3 + x2,
+ x0*x1*x2 + x0*x1*x3 + x0*x3 + x0 + x1*x2*x3 + x3 + 1,
+ x0*x1*x3 + x0*x1 + x1*x2*x3 + x1*x3 + x2*x3 + 1]:
+    print("The ANF of Midori_Sb0 is computed correctly")
+
+test=[]
+for i in range(16):
+    t3,t2,t1,t0=to_bin(i,4)
+    test.append(from_bin([anf_Sb0[j](t0,t1,t2,t3) for j in [3,2,1,0]]))
+
+if Sb0.lut()==test:
+    print("The polynomials can be evaluated to retrieve the LUT")
