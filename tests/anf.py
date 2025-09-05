2bdb4a90c6c04c85a4677cd7ea2b5df1fa0e65a0
Leo Perrin <leo.perrin@inria.fr>
2025-09-05 09:36:18 +0300

    printing a bit more stuff in tests/anf.py

--- tests/anf.py
+++ tests/anf.py
@@ -1,5 +1,5 @@
 from sage.all import *
-from sboxUv2.core import *
+from sboxUv2 import *
 from sage.crypto.sboxes import sboxes
 
 
@@ -7,11 +7,15 @@ Sb0=Sb(sboxes["Midori_Sb0"])
 anf_Sb0=algebraic_normal_form(Sb0)
 R = PolynomialRing(GF(2), names=('x0', 'x1', 'x2','x3',))
 (x0,x1,x2,x3,) = R._first_ngens(4)
+for coord in anf_Sb0:
+    print(coord)
+pprint(degree_spectrum(Sb0))
 if anf_Sb0==[x0*x1*x2 + x0*x1*x3 + x0*x2 + x0*x3 + x1*x2*x3 + x1,
  x0*x2 + x0*x3 + x0 + x2*x3 + x2,
  x0*x1*x2 + x0*x1*x3 + x0*x3 + x0 + x1*x2*x3 + x3 + 1,
  x0*x1*x3 + x0*x1 + x1*x2*x3 + x1*x3 + x2*x3 + 1]:
     print("The ANF of Midori_Sb0 is computed correctly")
+
 
 test=[]
 for i in range(16):
