a3f5945a0c383ee20b844392056bc0f9c649b4f1
Leo Perrin <leo.perrin@inria.fr>
2025-09-05 09:53:03 +0300

    tweaks to ANF

--- tests/anf.py
+++ tests/anf.py
@@ -5,11 +5,15 @@ from sage.crypto.sboxes import sboxes
 
 Sb0=Sb(sboxes["Midori_Sb0"])
 anf_Sb0=algebraic_normal_form(Sb0)
-R = PolynomialRing(GF(2), names=('x0', 'x1', 'x2','x3',))
-(x0,x1,x2,x3,) = R._first_ngens(4)
+
+# the coordinates of the ANF (such as the first one) are SAGE polynomials, meaning their `parent()` is the multivariate polynomial over which they are defined.
+R = anf_Sb0[0].parent()
+(x0,x1,x2,x3,) = R.gens()
+
 for coord in anf_Sb0:
     print(coord)
 pprint(degree_spectrum(Sb0))
+
 if anf_Sb0==[x0*x1*x2 + x0*x1*x3 + x0*x2 + x0*x3 + x1*x2*x3 + x1,
  x0*x2 + x0*x3 + x0 + x2*x3 + x2,
  x0*x1*x2 + x0*x1*x3 + x0*x3 + x0 + x1*x2*x3 + x3 + 1,
