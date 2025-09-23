3d6c30d76b17a01cc7529443c4e43c380f1df45a
Merlin Fruchon <mfruchon@ptb-02009352.paris.inria.fr>
2025-09-23 17:20:30 +0200

    Ajout du test de construction de la boîte S de Scream

--- tests/building_blocks.py
+++ tests/building_blocks.py
@@ -14,3 +14,13 @@ if __name__ == "__main__":
         print("Succeeded")
     else : 
         print("Failed") 
+
+    print("Constructing Scream's Sbox as a Feistel")
+    SW=swap_halves(8)
+    F1=Sb([0,2,0,0xb,3,0,0,0xa,1,0xe,0,6,0xa,4,5,2])
+    F2=Sb([0,2,0xc,7,5,0xf,0xd,6,4,0xe,8,9,3,1,0xb,0xa])
+    F3=Sb([2,0,0xb,0,0,3,0xa,0,0xe,1,6,0,4,0xa,2,5])
+    if feistel_round(F3)*feistel_round(F2)*feistel_round(F1)*SW==Sb(sboxes["Scream"]):
+        print("Succeeded")
+    else : 
+        print("Failed") 
