99f8d2a63216800841bf493c007ac86713473a23
Jules Baudrin <jules.baudrin@inria.fr>
2025-11-04 07:27:45 +0100

    Modify cpp_equivalences_from_lat. Now take sboxes as inputs (instead of LATs), return a Blm (instead of 4 blocks as blm) and handle verifications before returning

--- sboxUv2/cpp/ccz/partition_preserving_linear_mapping/pplm.cpp
+++ sboxUv2/cpp/ccz/partition_preserving_linear_mapping/pplm.cpp
@@ -1,73 +1,91 @@
 //
 // Created by Jules Baudrin on 19/06/2025.
-// Modified by Jules Baudrin on 21/10/2025.
+// Modified by Jules Baudrin on 03/11/2025.
 //
 #include "pplm.hpp"
 
-vector<vector<cpp_BinLinearMap>> cpp_equivalences_from_lat(
-    const vector<vector<Integer>> &lat1,
-    const vector<vector<Integer>> &lat2,
+
+bool cpp_verify_el_equivalence(cpp_S_box *sbox1,  cpp_S_box *sbox2, const cpp_BinLinearMap &solution) {
+    vector<cpp_BinLinearMap> ABCD = cpp_ccz_block_decomposition(solution);
+ 	cpp_S_box A = ABCD[0].get_cpp_S_box();
+    cpp_S_box B = ABCD[1].get_cpp_S_box();
+    cpp_S_box C = ABCD[2].get_cpp_S_box();
+    cpp_S_box D = ABCD[3].get_cpp_S_box();
+    
+    for (int i = 0; i < sbox1->size(); ++i) {
+    	if(D[i])
+    		return false;
+    }
+    auto sbox1_prime = (B * (*sbox1)) + C;
+    auto sbox2_prime = (*sbox2) * A;
+    cout << (sbox1_prime == sbox2_prime) << endl;q
+    return sbox1_prime == sbox2_prime;
+}
+
+
+vector<cpp_BinLinearMap> cpp_equivalences_from_lat(
+    cpp_S_box sbox1,
+    cpp_S_box sbox2,
     const bool &single_non_trivial_answer,
     const unsigned int &number_of_threads,
     const string equivalence_type)
 {
-	Integer n = log2(lat1.size());
+	Integer n = log2(sbox1.size());
 	BinWord size = (ONE << n);
+	vector<vector<Integer>> lat1 = cpp_lat(sbox1);
+	vector<vector<Integer>> lat2 = cpp_lat(sbox2);
 
-	map <pair<Integer, string>, vector<BinWord>> dict1;
-	map <pair<Integer, string>, vector<BinWord>> dict2;
+	map<pair<Integer, string>, vector<BinWord>> dict1;
+	map<pair<Integer, string>, vector<BinWord>> dict2;
 	for(BinWord x = 0; x < size; x++) {
 		for(BinWord y = 0; y < size; y++) {
-			if(equivalence_type == "ccz-linear" || (x != 0 && y != 0)) { // Either we search for ccz equivalences and all (x, y) are mapped in "mixed" buckets
+			if(equivalence_type == "ccz-linear" || equivalence_type == "extended-linear") { // Either we search for ccz equivalences and all (x, y) are mapped in "mixed" buckets
 				dict1[{lat1[x][y], "mixed"}].push_back((y << n) ^ x);
 				dict2[{lat2[x][y], "mixed"}].push_back((y << n) ^ x);
 			}
-			else { // Or we search for EL or L equivalences
-				if(x == 0) {// Otherwise, split the buckets so that (x, 0) is necessarily mapped onto (x', 0) (and the same for (0, y) -> (0, y') in the case of linear equivalence).
+			if(equivalence_type == "linear") {
+				if(x == 0) { 
 					dict1[{lat1[x][y], "x=0"}].push_back((y << n) ^ x);
 					dict2[{lat2[x][y], "x=0"}].push_back((y << n) ^ x);
 				}
-				else { // y == 0
-					if(equivalence_type == "linear") {
-						dict1[{lat1[x][y], "y=0"}].push_back((y << n) ^ x);
-						dict2[{lat2[x][y], "y=0"}].push_back((y << n) ^ x);
-					}
-					else {
-						dict1[{lat1[x][y], "mixed"}].push_back((y << n) ^ x);
-						dict2[{lat2[x][y], "mixed"}].push_back((y << n) ^ x);
-					}
+				else if(y == 0) {
+					dict1[{lat1[x][y], "y=0"}].push_back((y << n) ^ x);
+					dict2[{lat2[x][y], "y=0"}].push_back((y << n) ^ x);
+				}
+				else {
+					dict1[{lat1[x][y], "mixed"}].push_back((y << n) ^ x);
+					dict2[{lat2[x][y], "mixed"}].push_back((y << n) ^ x);
 				}
 			}
+			// if(equivalence_type == "extended-linear") {
+			// 	if(y == 0) { 
+			// 		dict1[{lat1[x][y], "y=0"}].push_back((y << n) ^ x);
+			// 		dict2[{lat2[x][y], "y=0"}].push_back((y << n) ^ x);
+			// 	}
+			// 	else {
+			// 		dict1[{lat1[x][y], "mixed"}].push_back((y << n) ^ x);
+			// 		dict2[{lat2[x][y], "mixed"}].push_back((y << n) ^ x);
+			// 	}
+			// }
 		}
-	}
+	}    
 
 	Partition<pair<Integer, string>> partition1(dict1, 2 * n);
 	Partition<pair<Integer, string>> partition2(dict2, 2 * n);
 
-	PartitionPreservingLinearMappings<pair<Integer, string>> pplm(&partition1, &partition2, single_non_trivial_answer, number_of_threads);
-	pplm.search();
-
-	vector<vector<cpp_BinLinearMap>> block_decompositions;
-	for(const auto &lut: pplm.found_mappings) {
-		// A D
-		// C B
-
-		vector<vector<BinWord>> image_vectors(4); // A B C D
-		BinWord v, w;
-		for (int i = 0; i < n; i++) {
-			v = lut[1 << i];
-			w = lut[1 << (n + i)];
-			image_vectors[0].push_back((v << n) >> n); //A
-			image_vectors[2].push_back(v >> n); // C
-
-			image_vectors[3].push_back((w << n) >> n); // D
-			image_vectors[1].push_back(w >> n); // B
-		}
-		vector<cpp_BinLinearMap> block_dec;
-		for (int i = 0; i < image_vectors.size(); i++) {
-			block_dec.push_back(cpp_BinLinearMap(image_vectors[i], n, n));
-		}
-		block_decompositions.push_back(block_dec);
+	PPExitCondition *exit_condition; 
+	PPEarlyAbortCondition *early_abort_condition;
+	if(equivalence_type == "extended-linear") {
+		exit_condition = new PPExitConditionEL(&sbox1, &sbox2);
+		early_abort_condition = new PPEarlyAbortConditionEL();
 	}
-	return block_decompositions;
+	else {
+		exit_condition = new PPExitConditionTrivial();
+		early_abort_condition = new PPEarlyAbortConditionTrivial();
+	}
+	
+		
+	PartitionPreservingLinearMappings<pair<Integer, string>> pplm = PartitionPreservingLinearMappings<pair<Integer, string>>(&partition1, &partition2, exit_condition, early_abort_condition, single_non_trivial_answer, number_of_threads);
+	pplm.search();
+	return pplm.found_mappings;
 }
