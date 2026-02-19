d2c1235e9d538d4f76c7f362cfd0635d6e254017
leo.perrin <perrin.leo@gmail.com>
2026-02-19 15:28:41 +0100

    fixed bad merge in pyproject and bugs in S_box

--- sboxUv2/ccz/affine_equivalence/__init__.py
+++ sboxUv2/ccz/affine_equivalence/__init__.py
@@ -4,7 +4,7 @@
 
 """
 
-from .cython_functions import \
+from sboxUv2.ccz.affine_equivalence.cython_functions import \
     xor_equivalence, \
     le_class_representative, linear_equivalence, linear_equivalence_permutations, \
     affine_equivalence_permutations, affine_equivalence
